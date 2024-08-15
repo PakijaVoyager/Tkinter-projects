c7aa2f995b25c8c72e79da20cc45c7dc92922d6a
Junat S'Imsuk <tanujkusmi1@gmail.com>
2024-08-15 08:02:28 +0530

    Update URL shortner.py

--- URL shortner.py
+++ URL shortner.py
@@ -6,17 +6,25 @@ root.title("Form")
 root.geometry("595x456")
 root.config(bg="light grey")
 
-def apply():
+def shorten_1():
     initiate = pyshorteners.Shortener()
     short_url = initiate.tinyurl.short(url_got.get())
     usr_ent.delete(0,END)
     usr_ent.insert(0,short_url)
-    print(short_url)
+    print("Shorten URL : ",short_url)
+
+def expand_1():
+    initiate = pyshorteners.Shortener()
+    expand_url = initiate.tinyurl.expand(url_got.get())
+    usr_ent.delete(0,END)
+    usr_ent.insert(0,expand_url)
+    print("Expand URL : ",expand_url)
 
 Label(root,text="Enter URL",font="Robote 21 bold",bg="light grey").pack(pady = 18)
 url_got = StringVar()
 usr_ent = Entry(root,width=30,borderwidth=5,font="arial 15 bold",relief="sunken",textvariable=url_got)
 usr_ent.pack()
 
-Button(root,text="Convert",borderwidth=6,width=9,font="lucida 15 bold",command=apply).pack(pady = 21)
+Button(root,text="Shorten",borderwidth=6,width=9,font="lucida 15 bold",command=shorten_1).pack(side = LEFT,padx = 115)
+Button(root,text="Expand",borderwidth=6,width=9,font="lucida 15 bold",command=expand_1).pack(side = LEFT)
 root.mainloop()
